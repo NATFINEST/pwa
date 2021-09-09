381a827b95d42d59c1464ecdf88dfe1ebc062339
NATFINEST <ese.nat4@gmail.com>
2021-09-09 02:21:21 +0100

    Enhancing the Installation Process

--- js/main.js
+++ js/main.js
@@ -2,21 +2,54 @@
 
 // https://flaviocopes.com/how-to-format-number-as-currency-javascript/
 const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-})
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
 
 function updateTipAmounts() {
-    // grab the meal cost from the page
-    let mealCost = document.getElementById("mealCost").value;
-    // populate the table with tip amounts
-    document.getElementById('tip10').innerHTML = formatter.format(mealCost * 0.10);
-    document.getElementById('tip15').innerHTML = formatter.format(mealCost * 0.15);
-    document.getElementById('tip18').innerHTML = formatter.format(mealCost * 0.18);
-    document.getElementById('tip20').innerHTML = formatter.format(mealCost * 0.20);
-    document.getElementById('tip22').innerHTML = formatter.format(mealCost * 0.22);
+  // grab the meal cost from the page
+  let mealCost = document.getElementById('mealCost').value;
+  // populate the table with tip amounts
+  document.getElementById('tip10').innerHTML = formatter.format(mealCost * 0.1);
+  document.getElementById('tip15').innerHTML = formatter.format(
+    mealCost * 0.15
+  );
+  document.getElementById('tip18').innerHTML = formatter.format(
+    mealCost * 0.18
+  );
+  document.getElementById('tip20').innerHTML = formatter.format(mealCost * 0.2);
+  document.getElementById('tip22').innerHTML = formatter.format(
+    mealCost * 0.22
+  );
 }
 
 // register the event listener for the input field
 document.getElementById('mealCost').oninput = updateTipAmounts;
+
+// Get a handle on the install button
+let installButton = document.getElementById('installButton');
+// Setthe click handler for the install button
+installButton.onclick = doInstall;
+
+// Create an object we'll use to hold the reference to the PWA install event
+let deferredPrompt;
+
+function doInstall() {
+  installButton.style.display = 'none';
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((res) => {
+    if (res.outcome === 'accepted') {
+      console.log('doInstall: accepted');
+    } else {
+      console.log('doInstall: declined');
+    }
+    deferredPrompt = null;
+  });
+}
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  installButton.style.display = 'block';
+});
